3aceb8cc2417872f63bbdb657a8e8360d118edd7
Pierrick brun <pierrick.brun@akretion.com>
2018-12-09 19:41:04 +0100

    [FIX] Allow duplicate gid

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -9,7 +9,7 @@ USERMAP_GID=$(stat -c %g /var/lib/postgresql/data)
 USERMAP_UID=$(stat -c %u /var/lib/postgresql/data)
 if [[ ${USERMAP_UID} != ${USERMAP_ORIG_UID} ]] || [[ ${USERMAP_GID} != ${USERMAP_ORIG_GID} ]]; then
   echo "Adapting uid and gid for ${PG_USER}:${PG_USER} to $USERMAP_UID:$USERMAP_GID"
-  groupmod -g ${USERMAP_GID} ${PG_USER}
+  groupmod -o -g ${USERMAP_GID} ${PG_USER}
   sed -i -e "s|:${USERMAP_ORIG_UID}:${USERMAP_GID}:|:${USERMAP_UID}:${USERMAP_GID}:|" /etc/passwd
 fi
 
