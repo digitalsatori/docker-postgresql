ed125e5af3b27180d3b10a2c14498d132f0e942f
Tony Gu <tony@openerp.cn>
2019-05-07 15:42:34 +0800

    remove uid gid map for buiding image on mac

--- Dockerfile
+++ Dockerfile
@@ -7,11 +7,10 @@ RUN DEBIAN_FRONTEND=noninteractive && \
     apt-get install -y postgresql-contrib && \
     apt-get clean
 
-COPY dev-docker-entrypoint.sh /
 COPY initdb.sh /docker-entrypoint-initdb.d/initdb.sh
 COPY postgresql.conf /etc/postgresql/postgresql.conf
 RUN chown postgres:postgres /etc/postgresql/postgresql.conf
-ENTRYPOINT ["/dev-docker-entrypoint.sh"]
+ENTRYPOINT ["/docker-entrypoint.sh"]
 
 EXPOSE 5432
 CMD ["postgres", "-c", "config_file=/etc/postgresql/postgresql.conf"]
--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
-PG_USER=postgres
-
-# Inpisred by https://github.com/sameersbn/docker-postgresql/blob/master/runtime/functions
-USERMAP_ORIG_UID=$(id -u ${PG_USER})
-USERMAP_ORIG_GID=$(id -g ${PG_USER})
-USERMAP_GID=$(stat -c %g /var/lib/postgresql/data)
-USERMAP_UID=$(stat -c %u /var/lib/postgresql/data)
-if [[ ${USERMAP_UID} != ${USERMAP_ORIG_UID} ]] || [[ ${USERMAP_GID} != ${USERMAP_ORIG_GID} ]]; then
-  echo "Adapting uid and gid for ${PG_USER}:${PG_USER} to $USERMAP_UID:$USERMAP_GID"
-  groupmod -g ${USERMAP_GID} ${PG_USER}
-  sed -i -e "s|:${USERMAP_ORIG_UID}:${USERMAP_GID}:|:${USERMAP_UID}:${USERMAP_GID}:|" /etc/passwd
-fi
+# PG_USER=postgres
+#
+# # Inpisred by https://github.com/sameersbn/docker-postgresql/blob/master/runtime/functions
+# USERMAP_ORIG_UID=$(id -u ${PG_USER})
+# USERMAP_ORIG_GID=$(id -g ${PG_USER})
+# USERMAP_GID=$(stat -c %g /var/lib/postgresql/data)
+# USERMAP_UID=$(stat -c %u /var/lib/postgresql/data)
+# if [[ ${USERMAP_UID} != ${USERMAP_ORIG_UID} ]] || [[ ${USERMAP_GID} != ${USERMAP_ORIG_GID} ]]; then
+#   echo "Adapting uid and gid for ${PG_USER}:${PG_USER} to $USERMAP_UID:$USERMAP_GID"
+#   groupmod -g ${USERMAP_GID} ${PG_USER}
+#   sed -i -e "s|:${USERMAP_ORIG_UID}:${USERMAP_GID}:|:${USERMAP_UID}:${USERMAP_GID}:|" /etc/passwd
+# fi
 
 ./docker-entrypoint.sh "$@"
